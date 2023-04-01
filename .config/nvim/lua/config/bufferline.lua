bf5dd5a73771191354a51a1b51306bd3542caaa3
Rejeq <jere3690@gmail.com>
2023-04-01 03:19:57 +0300

    nvim: buffer line icons are explicit written, because they did not appear, possibly due to a plugin update

--- .config/nvim/lua/config/bufferline.lua
+++ .config/nvim/lua/config/bufferline.lua
@@ -7,6 +7,16 @@ local options = {
     text_align = "center",
     separator = true
   } },
+
+  indicator = {
+    icon = '▎', -- this should be omitted if indicator style is not 'icon'
+    style = 'icon' ,
+  },
+  buffer_close_icon = '',
+  modified_icon = '●',
+  close_icon = '',
+  left_trunc_marker = '',
+  right_trunc_marker = '',
 }
 
 bufferline.setup { options = options }
