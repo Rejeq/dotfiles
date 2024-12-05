11d2c972fb161c38e93be4075a9e44928098b677
Rejeq <jere3690@gmail.com>
2024-12-05 22:13:43 +0300

    nvim: Fix slow statusline

--- .config/nvim/lua/config/lualine.lua
+++ .config/nvim/lua/config/lualine.lua
@@ -1,22 +1,10 @@
 local lualine = require('lualine')
 
 local options = {
-  icons_enabled = true,
-  theme = 'auto',
   component_separators = { left = '', right = '' },
   section_separators = { left = '', right = '' },
-  disabled_filetypes = {
-    statusline = {},
-    winbar = {},
-  },
-  ignore_focus = {},
   always_divide_middle = true,
   globalstatus = true,
-  refresh = {
-    statusline = 1000,
-    tabline = 1000,
-    winbar = 1000,
-  }
 }
 
 local sections = {
